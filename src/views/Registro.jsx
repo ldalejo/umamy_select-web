3274d60bc49932f88243b9b81e37be86e1d284ed
ldalejo <ldalejo.dev@gmail.com>
2024-12-08 19:47:31 +0100

    Creación de vista registro con estilos

--- src/views/Registro.jsx
+++ src/views/Registro.jsx
@@ -2,8 +2,82 @@ import React from 'react'
 
 export default function Registro() {
   return (
-    <div>
-        Registro
-    </div>
+    <>
+        <h1>Crear cuenta</h1>
+        <div className="formulario-contenedor">
+            <form>
+                <div className="formulario-apartado">
+                    <label
+                        className="formulario-label"
+                        htmlFor="nombre"
+                    >
+                        Nombre:
+                    </label>
+                    <input 
+                        type="text" 
+                        id="nombre"
+                        className="formulario-input"
+                        name="nombre"
+                        placeholder="Nombre"
+                    />
+                </div>
+
+                <div className="formulario-apartado">
+                    <label
+                        className="formulario-label"
+                        htmlFor="email"
+                    >
+                        Email:
+                    </label>
+                    <input 
+                        type="email" 
+                        id="email"
+                        className="formulario-input"
+                        name="email"
+                        placeholder="Email"
+                    />
+                </div>
+
+                <div className="formulario-apartado">
+                    <label
+                        className="formulario-label"
+                        htmlFor="password"
+                    >
+                        Password:
+                    </label>
+                    <input 
+                        type="password" 
+                        id="password"
+                        className="formulario-input"
+                        name="password"
+                        placeholder="Password"
+                    />
+                </div>
+
+                <div className="formulario-apartado">
+                    <label
+                        className="formulario-label"
+                        htmlFor="password_confirmation"
+                    >
+                        Repetir Password:
+                    </label>
+                    <input 
+                        type="password" 
+                        id="password_confirmation"
+                        className="formulario-input"
+                        name="password_confirmation"
+                        placeholder="Repetir Password"
+                    />
+                </div>
+
+                <button
+                    className='formulario-boton'
+                >
+                    Continuar
+                </button>
+
+            </form>
+        </div>
+    </>
   )
 }
