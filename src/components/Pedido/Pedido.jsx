45f890104f3b4c90847d87576aa96ec017ea54a3
DESKTOP-PAOMC4B\David <ldalejo.dev@gmail.com>
2025-02-21 20:54:14 +0100

    Creado componente Pedido

--- src/components/Pedido/Pedido.jsx
+++ src/components/Pedido/Pedido.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
 
+import './Pedido.css'
+
 export default function Resumen() {
+
   return (
-    <div className='pedido'>Pedido</div>
+    <section className='pedido'>
+      <article >
+        <h1>Tu Pedido</h1>
+        <p>Tu carrito está vacio</p>
+      </article>
+      <article>
+        <p>Importe total : </p>
+      </article>
+    </section>
   )
 }
