1c8e51b479450ccf5611544e15bc68728367602c
DESKTOP-PAOMC4B\David <ldalejo.dev@gmail.com>
2025-03-08 19:28:28 +0100

    añadido provider al main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
+import { UmamyProvider } from './context/UmamyProvider'
+
 import router from './router'
 
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <UmamyProvider>
+      <RouterProvider router={router} />
+    </UmamyProvider>
   </StrictMode>
 )
