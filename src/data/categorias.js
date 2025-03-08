0df73754f7d13136062a514a7ba66f9ee97b3262
DESKTOP-PAOMC4B\David <ldalejo.dev@gmail.com>
2025-03-08 19:30:09 +0100

    cambios en los estilos, cambio de etiquetas, añadido hook useUmamy

--- src/data/categorias.js
+++ src/data/categorias.js
@@ -10,19 +10,24 @@ const categorias = [
         id: 2
       },
       {
+        //icono: "/img/icono_pizzas.png",
+        nombre: "Pizzas",
+        id: 3
+      },
+      {
         icono: "/img/icono_carnes.png",
         nombre: "Carnes",
-        id: 3
+        id: 4
       },
       {
         icono: "/img/icono_bebidas.png",
         nombre: "Bebidas",
-        id: 4
+        id: 5
       },
       {
         icono: "/img/icono_postres.png",
         nombre: "Postres",
-        id: 5
+        id: 6
       }
 ]
 
