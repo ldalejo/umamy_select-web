9742ed52b30c0bc7f59d76780432e08b0745eacb
DESKTOP-PAOMC4B\David <ldalejo.dev@gmail.com>
2025-03-09 11:21:53 +0100

    Cambios en la semántica html para hacerlo más accesible. Modificación css

--- src/components/ModalProducto/ModalProducto.jsx
+++ src/components/ModalProducto/ModalProducto.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { MdCancel, MdAddCircle, MdRemoveCircle  } from "react-icons/md";
 import useUmamy from '../../hooks/useUmamy';
 import { formatearDinero } from '../../helpers';
@@ -9,8 +9,14 @@ import './ModalProducto.css'
 export default function ModalProducto() {
 
     const [cantidad, setCantidad] = useState(1);
+    const [importe, setImporte] = useState(0);
 
     const { producto, handleClickModal } = useUmamy();
+
+    /* Tenemos el importe siempre actualizado a la cantidad de producto*/
+    useEffect(() => {
+        actualizarImporte();
+    }, [cantidad])
 
     const restarProducto = () => {
         if (cantidad > 1) {
@@ -24,46 +30,53 @@ export default function ModalProducto() {
         }
     }
 
+    const actualizarImporte = () => {
+        setImporte(cantidad * producto.precio);
+    }
+
   return (
-    <div>
+    <div role="dialog" aria-modal="true">
         <button
             onClick={handleClickModal}
-            className='modal__icono-cerrar'
+            className="modal__icono-cerrar"
+            aria-label="Cerrar modal"
         >
-            <MdCancel  size={40} />
+            <MdCancel size={40} />
         </button>
-        <div>
+    <section>
             <h2>{producto.nombre}</h2>
-            <img 
-                alt={`Imagen producto ${producto.nombre}`} 
-                src={`${producto.imagen}`}
-                className='imagen'
-            />
-            <p>{formatearDinero(producto.precio)}</p>
-            <div className='modal__cantidad-container'>
+        <img 
+            alt={`Imagen producto ${producto.nombre}`} 
+            src={producto.imagen}
+            className="imagen"
+        />
+      <section className="modal__botones-container">
+            <article className="modal__cantidad-container">
                 <button
-                    type='button'
-                    className='modal_sumar-restar'
+                    type="button"
+                    className="modal_sumar-restar"
                     onClick={restarProducto}
+                    aria-label="Disminuir cantidad"
                 >
-                    <MdRemoveCircle/>
+                    <MdRemoveCircle />
                 </button>
-                <p className='modal__parrafo'>{cantidad}</p>
+                <output className="modal__parrafo" aria-live="polite">
+                    {cantidad}
+                </output>
                 <button
-                    type='button'
-                    className='modal_sumar-restar'
+                    type="button"
+                    className="modal_sumar-restar"
                     onClick={sumarProducto}
+                    aria-label="Aumentar cantidad"
                 >
-                    <MdAddCircle/>
+                <MdAddCircle />
                 </button>
-                <button
-                    type='button'
-                    className='modal__anadir-producto'
-                >
-                    Añadir
-                </button>
-            </div>
-        </div>
-    </div>
+            </article>
+            <button type="button" className="modal__anadir-producto">
+                Añadir {formatearDinero(importe)}
+            </button>
+        </section>
+    </section>
+  </div>
   )
 }
