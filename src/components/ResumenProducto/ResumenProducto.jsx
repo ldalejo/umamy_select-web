c93f853f1b35778d2ccb8513650bc25761dc37a1
DESKTOP-PAOMC4B\David <ldalejo.dev@gmail.com>
2025-03-09 17:49:00 +0100

    editado css ResumenProducto. Añadida funcion actualizarImporte en este componente

--- src/components/ResumenProducto/ResumenProducto.jsx
+++ src/components/ResumenProducto/ResumenProducto.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import { MdEdit,  MdDelete} from "react-icons/md";
 import { formatearDinero } from '../../helpers';
+import { useState, useEffect } from 'react';
 
 import './ResumenProducto.css'
 
 export default function ResumenProducto({producto}) {
+  const [importe, setImporte] = useState(0);
+  
+  const { id, nombre, imagen, precio, cantidad } = producto;
 
-    const { id, nombre, imagen, precio, cantidad } = producto;
+  /* Tenemos el importe siempre actualizado a la cantidad de producto*/
+  useEffect(() => {
+      actualizarImporte();
+  }, [cantidad]);
+
+  const actualizarImporte = () => {
+    setImporte(cantidad * producto.precio);
+}
 
   return (
     <section className='resumenPedido__container'>
@@ -24,20 +35,20 @@ export default function ResumenProducto({producto}) {
             <p className='resumenPedido__p'>{cantidad}</p>
             <div className='resumenPedido__container-botones'>
                 <button 
-                    className='resumenPedido__iconos'
+                    className='resumenPedido__icono-editar'
                     aria-label='Editar producto'
                 >
                     <MdEdit aria-hidden="true"/>
                 </button>
                 <button 
-                    className='resumenPedido__iconos'
+                    className='resumenPedido__icono-eliminar'
                     aria-label='Eliminar producto'
                 >
                     <MdDelete aria-hidden="true"/>
                 </button>
             </div>
         </section>
-        <p className='resumenPedido__precio-p'>{formatearDinero(precio)}</p>
+        <p className='resumenPedido__precio-p'>{formatearDinero(importe)}</p>
       </article>
     </section>
   )
