84dadf4543b6044f270d4fdaf0f89b9da7f5a75d
DESKTOP-PAOMC4B\David <ldalejo.dev@gmail.com>
2025-03-16 20:55:35 +0100

    creada función para el registro de usuarios. Eliminados label en el formulario de registro. Añadidas alertas de errores en el formulario

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,11 +2,11 @@ import axios from "axios";
 
 const Axios = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
-/*     headers: {
+    headers: {
         'Accept' : 'application/json',
         'X-Requested-With': 'XMLHttpRequest'
     },
-    withCredentials: true */
+    withCredentials: true
 })
 
 export default Axios
