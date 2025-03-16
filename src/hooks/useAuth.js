bcb5b7722d48bfbab24800a0e840e61f63b39cd3
DESKTOP-PAOMC4B\David <ldalejo.dev@gmail.com>
2025-03-17 00:15:29 +0100

    Añadiendo restricción para que solo los usuarios puedan entrar en la web

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -64,12 +64,18 @@ const useAuth = ({middleware, url}) => {
         if (middleware === 'guest' && url && user) {
             navigate(url);
         }
+
+        if (middleware === 'auth' && error) {
+            navigate('/auth/login');
+        }
     },[user, error]);
 
     return {
         login,
         registro,
-        logout
+        logout,
+        user,
+        error
     }
 };
 
