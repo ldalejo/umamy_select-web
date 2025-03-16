0af53fccfc589dfe1f67ff5634822d3a7a1005c5
DESKTOP-PAOMC4B\David <ldalejo.dev@gmail.com>
2025-03-16 21:31:12 +0100

    creada función para el login de usuarios. Eliminados label en el formulario de login. Añadidas alertas de errores en el formulario

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,41 +1,81 @@
 import React from 'react'
+import { createRef, useState } from 'react'
 import { Link } from 'react-router-dom'
+import Axios from '../config/axios';
+import AlertaErroresFormulario from '../components/AlertaErroresFormulario/AlertaErroresFormulario';
 
 export default function Login() {
+
+  const [errores, setErrores] = useState();
+
+    const emailRef = createRef();
+    const passwordRef = createRef();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+
+        const datos = {
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+        }
+
+        try {
+          const { data } = await Axios.post('/api/iniciar-sesion', datos);
+          localStorage.setItem('AUTH_TOKEN', data.token);
+          setErrores([]);
+        } catch (error) {
+          setErrores(Object.values(error.response.data.errors));
+        }
+    }
+
   return (
     <>
         <h1>Iniciar sesión</h1>
         <div className="formulario-contenedor">
-            <form>
+            <form
+              onSubmit={handleSubmit}
+              noValidate
+            >
+              {
+                errores 
+                ? 
+                errores.map(error => 
+                    <AlertaErroresFormulario key={error}>{error}</AlertaErroresFormulario>
+                ) 
+                : 
+                null
+              }
               <div className="formulario-apartado">
-                <label
+                {/* <label
                     className="formulario-label"
                     htmlFor="email"
                 >
                     Email:
-                </label>
+                </label> */}
                 <input 
                     type="email" 
                     id="email"
                     className="formulario-input"
                     name="email"
                     placeholder="Email"
+                    ref={emailRef}
                 />
               </div>
 
               <div className="formulario-apartado">
-                <label
+                {/* <label
                     className="formulario-label"
                     htmlFor="password"
                 >
                     Password:
-                </label>
+                </label> */}
                 <input 
                     type="password" 
                     id="password"
                     className="formulario-input"
                     name="password"
                     placeholder="Password"
+                    ref={passwordRef}
                 />
               </div>
 
