19af4e71e092369e6a47a1e1fe885eaf95a4845c
DESKTOP-PAOMC4B\David <ldalejo.dev@gmail.com>
2025-03-18 07:36:22 +0100

    Modificado componente producto para poder reutilizarlo en la vista de administrador

--- src/components/Producto/Producto.jsx
+++ src/components/Producto/Producto.jsx
@@ -4,34 +4,53 @@ import useUmamy from '../../hooks/useUmamy';
 
 import './Producto.css'
 
-export default function Producto({producto}) {
+export default function Producto({producto, botonAgregar = false, botonDisponibilidad = false}) {
 
-    const { handleClickModal, handleSetProducto } = useUmamy();
+    const { handleClickModal, handleSetProducto, handleDisponibilidadProducto } = useUmamy();
     // Aplicamos destructuring
     const {id, imagen, nombre, precio} = producto;
 
     return (
-        <article className='producto'>
-            <img 
-                src={imagen} 
-                alt={`imagen ${nombre}`}
-                className='imagen'
-            />
-            <section className='producto__contenido'>
-                <p className='contenido__nombre'>{nombre}</p>
-                <p className='contenido__precio'>{formatearDinero(precio)}</p>
-                <button
-                    type='button'
-                    className='producto__boton'
-                    onClick={() => {
-                        handleClickModal();
-                        handleSetProducto(producto);
-                    }
-                    }
-                >
-                    PEDIR
-                </button>
-            </section>
-        </article>
-    )
+      <article className="producto">
+        <img 
+            src={imagen} 
+            alt={`imagen ${nombre}`} 
+            className="imagen" 
+        />
+        <section className="producto__contenido">
+          <p className="contenido__nombre">{nombre}</p>
+          <p className="contenido__precio">{formatearDinero(precio)}</p>
+          {botonAgregar && (
+            <button
+              type="button"
+              className="producto__boton"
+              onClick={() => {
+                handleClickModal();
+                handleSetProducto(producto);
+              }}
+            >
+              PEDIR
+            </button>
+          )}
+          {botonDisponibilidad && (
+            <button
+              type="button"
+              className="producto__boton"
+              onClick={() => handleDisponibilidadProducto(producto.id)}
+            >
+              DISPONIBLE
+            </button>
+          )}
+          {!botonDisponibilidad && !botonAgregar && (
+            <button
+              type="button"
+              className="producto__boton"
+              onClick={() => handleDisponibilidadProducto(producto.id)}
+            >
+              AGOTADO
+            </button>
+          )}
+        </section>
+      </article>
+    );
 }
