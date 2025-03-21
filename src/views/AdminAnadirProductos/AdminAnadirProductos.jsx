9332b9e4651adfe7fb167bb0d9afe56ee63c64eb
DESKTOP-PAOMC4B\David <ldalejo.dev@gmail.com>
2025-03-21 08:08:48 +0100

    Corrigiendo código innecesario

--- src/views/AdminAnadirProductos/AdminAnadirProductos.jsx
+++ src/views/AdminAnadirProductos/AdminAnadirProductos.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
-import useUmamy from "../../hooks/useUmamy";
-import useSWR from "swr";
+import useUmamy from '../../hooks/useUmamy';
 import { useForm } from 'react-hook-form';
 import * as Yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -19,21 +18,21 @@ export default function AdminAnadirProductos() {
 
     // Esquema de validación con Yup
     const validacion = Yup.object({
-        categoria: Yup.string()
+        categoria_id: Yup.string()
             .required('Seleccione una categoría.'),
         nombre: Yup.string()
             .required('El nombre es obligatorio'),
         precio: Yup.number()
-            .typeError("El precio debe ser un número válido")
-            .positive("El precio debe ser mayor a 0")
-            .max(100, "El precio no puede superar los 100€")
+            .typeError('El precio debe ser un número válido')
+            .positive('El precio debe ser mayor a 0')
+            .max(100, 'El precio no puede superar los 100€')
             .required('El precio es obligatorio'),
         imagen: Yup.mixed()
-            .test("required", "Debes subir una imagen", (value) => {
+            .test('required', 'Debes subir una imagen', (value) => {
                 return value && value.length > 0;
             })
-            .test("fileType", "Solo se permiten imágenes (jpg, png, jpeg)", (value) => {
-                return value && value[0] && ["image/jpeg", "image/png", "image/jpg"].includes(value[0].type);
+            .test('fileType', 'Solo se permiten imágenes (jpg, png, jpeg)', (value) => {
+                return value && value[0] && ['image/jpeg', 'image/png', 'image/jpg'].includes(value[0].type);
             })
     });
 
@@ -42,15 +41,6 @@ export default function AdminAnadirProductos() {
         resolver: yupResolver(validacion),
         mode: 'onBlur',
     });
-
-    const fetcher = () =>
-        Axios.get('/api/producto/anadir-producto', {
-            headers: {
-            Authorization: `Bearer ${token}`,
-            },
-    });
-
-    const { data, error, isLoading } = useSWR('/api/producto/anadir-producto', fetcher, { revalidateOnFocus: false });
 
     const onSubmit = async (data) => {
         setDisabled(true);
@@ -72,19 +62,19 @@ export default function AdminAnadirProductos() {
     return (
         <>
             <h1 className='h1__producto'>Añadir nuevo producto</h1>
-            <section className="formulario-producto__contenedor">
-                <form onSubmit={handleSubmit(onSubmit)} className="formulario-producto-grid" encType="multipart/form-data">
-                    <div className="formulario-producto__grupo">
-                        <label className="formulario-producto__label">Elige una categoria:</label>
+            <section className='formulario-producto__contenedor'>
+                <form onSubmit={handleSubmit(onSubmit)} className='formulario-producto-grid' encType='multipart/form-data'>
+                    <div className='formulario-producto__grupo'>
+                        <label className='formulario-producto__label'>Elige una categoria:</label>
                         <select
-                            name="categoria_id"
-                            placeholder="Categoria"
-                            className="formulario-producto__input"
+                            name='categoria_id'
+                            placeholder='Categoria'
+                            className='formulario-producto__input'
                             {...register('categoria_id')}
                         >
                             <option 
-                                value="" 
-                                className="formulario-producto__input"
+                                value='' 
+                                className='formulario-producto__input'
                             >
                                 Seleccione una categoría
                             </option>
@@ -93,7 +83,7 @@ export default function AdminAnadirProductos() {
                                     <option 
                                         key={categoria.id} 
                                         value={categoria.id} 
-                                        className="formulario-producto__input"
+                                        className='formulario-producto__input'
                                     >
                                         {categoria.nombre}
                                     </option>
@@ -103,44 +93,44 @@ export default function AdminAnadirProductos() {
                         {errors.categoria && <AlertaErroresFormulario>{errors.categoria.message}</AlertaErroresFormulario>}
                     </div>
 
-                    <div className="formulario-producto__grupo">
-                        <label className="formulario-producto__label">Nombre producto:</label>
+                    <div className='formulario-producto__grupo'>
+                        <label className='formulario-producto__label'>Nombre producto:</label>
                         <input 
-                            type="text" 
-                            name="nombre"
-                            placeholder="Nombre del producto"
-                            className="formulario-producto__input"
+                            type='text' 
+                            name='nombre'
+                            placeholder='Nombre del producto'
+                            className='formulario-producto__input'
                             {...register('nombre')}
                         />
                         {errors.nombre && <AlertaErroresFormulario>{errors.nombre.message}</AlertaErroresFormulario>}
                     </div>
 
-                    <div className="formulario-producto__grupo">
-                        <label className="formulario-producto__label">Precio:</label>
+                    <div className='formulario-producto__grupo'>
+                        <label className='formulario-producto__label'>Precio:</label>
                         <input 
-                            type="number" 
-                            name="precio"
-                            placeholder="Precio del producto"
-                            className="formulario-producto__input-distinto"
+                            type='number' 
+                            name='precio'
+                            placeholder='Precio del producto'
+                            className='formulario-producto__input-distinto'
                             {...register('precio')}
                         />
                         {errors.precio && <AlertaErroresFormulario>{errors.precio.message}</AlertaErroresFormulario>}
                     </div>
 
-                    <div className="formulario-producto__grupo">
-                        <label className="formulario-producto__label">Guarda una imagen:</label>
+                    <div className='formulario-producto__grupo'>
+                        <label className='formulario-producto__label'>Guarda una imagen:</label>
                         <input 
-                            type="file" 
-                            name="imagen"
-                            className="formulario-producto__input-distinto"
+                            type='file' 
+                            name='imagen'
+                            className='formulario-producto__input-distinto'
                             {...register('imagen')}
                         />
                         {errors.imagen && <AlertaErroresFormulario>{errors.imagen.message}</AlertaErroresFormulario>}
                     </div>
 
-                    <div className="formulario-producto__contenedor-button">
+                    <div className='formulario-producto__contenedor-button'>
                         <button 
-                            className="formulario-producto__boton" 
+                            className='formulario-producto__boton' 
                             disabled={disabled}
                         >
                             {valueButton}
